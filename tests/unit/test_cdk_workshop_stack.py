6990413dcf735d6139463f54e63c92e0b9370fc2
Juan Manuel Ruiz Fernández <jruizferprofesional@gmail.com>
2020-08-11 18:11:40 +0200

    [tests] Added tests setup.

--- tests/unit/test_cdk_workshop_stack.py
+++ tests/unit/test_cdk_workshop_stack.py
@@ -1,15 +1,20 @@
 import json
+import pytest
 
 from aws_cdk import core
 from cdk_workshop.cdk_workshop_stack import CdkWorkshopStack
 
-def get_template():
+@pytest.fixture(scope = 'module')
+def global_data():
   app = core.App()
   CdkWorkshopStack(app, "cdk-workshop-dev")
-  return json.dumps(app.synth().get_stack("cdk-workshop-dev").template)
+  return {'template': json.dumps(app.synth().get_stack("cdk-workshop-dev").template)}
 
-def test_lambda_function_created():
-  assert "AWS::Lambda::Function" in get_template()
+def test_lambda_restapi_created(global_data):
+  assert "AWS::ApiGateway::RestApi" in global_data["template"]
 
-def test_dynamodb_table_created():
-  assert "AWS::DynamoDB::Table" in get_template()
+def test_lambda_function_created(global_data):
+  assert "AWS::Lambda::Function" in global_data["template"]
+
+def test_dynamodb_table_created(global_data):
+  assert "AWS::DynamoDB::Table" in global_data["template"]
